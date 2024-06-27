9dc5ad9bb7a037298ae4193d09af62ce58a09101
Анастасия <citadeln@mail.ru>
2024-06-27 16:15:55 +0500

    NATURAL JOIN

---
+++ src/ex08/day01_ex08.sql
@@ -0,0 +1,5 @@
+SELECT order_date,
+       CONCAT(person.name, ' (age:', age, ')') AS person_information
+    FROM person_order AS person_order(person_id, id)
+NATURAL JOIN person
+ORDER BY order_date, person_information;
