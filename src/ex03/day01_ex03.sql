d266ec48b6afba863343145b78915b1c6ec7eb42
Анастасия <citadeln@mail.ru>
2024-06-27 14:24:56 +0500

    ex03

---
+++ src/ex03/day01_ex03.sql
@@ -0,0 +1,8 @@
+SELECT order_date AS acion_date,
+       person_id AS person_id
+FROM person_order
+INTERSECT
+SELECT visit_date,
+       person_id
+FROM person_visits
+ORDER BY acion_date, person_id DESC
