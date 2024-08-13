6d02bf90adb9ad77ee08ae6d118ab0f1f432e7e0
Анастасия <citadeln@mail.ru>
2024-08-13 12:57:49 +0500

    add sorting

--- SQL_beginner.Day03-1/src/ex00/day03_ex00.sql
+++ SQL_beginner.Day03-1/src/ex00/day03_ex00.sql
@@ -1,11 +1,12 @@
 SELECT  pizza_name,
         price,
         pizzeria.name AS pizzeria_name,
-        vizit_date
+        visit_date
 FROM menu
 INNER JOIN pizzeria ON menu.pizzeria_id = pizzeria.id
 INNER JOIN person_visits ON pizzeria.id = person_visits.pizzeria_id
 INNER JOIN person ON person_visits.person_id = person.id
 WHERE   person.name = 'Kate' 
-        AND price BETWEEN 800 AND 1000;
+        AND price BETWEEN 800 AND 1000
+ORDER BY 1, 2, 3;
         
