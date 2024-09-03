b6459906fdec7dd9d9dc9d491bea0f940d1ec102
Анастасия <citadeln@mail.ru>
2024-09-03 20:43:14 +0500

    ex00 CREATE INDEX B-tree

---
+++ SQL_beginner.Day05-1/ex00/day05_ex00.sql
@@ -0,0 +1,7 @@
+CREATE INDEX IF NOT EXISTS idx_menu_pizzeria_id ON menu (pizzeria_id);
+CREATE INDEX IF NOT EXISTS idx_person_order_person_id ON person_order(person_id);
+CREATE INDEX IF NOT EXISTS idx_person_order_menu_id ON person_order(menu_id);
+CREATE INDEX IF NOT EXISTS idx_person_visits_person_id ON person_visits(person_id);
+CREATE INDEX IF NOT EXISTS idx_person_visits_pizzeria_id ON person_visits(pizzeria_id);
+
+--SELECT * FROM pg_indexes;
