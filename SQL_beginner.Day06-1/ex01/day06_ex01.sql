2deb0301020cf2fd4f93781be559a5cfd45a9b14
Анастасия <citadeln@mail.ru>
2024-09-08 19:36:55 +0500

    ex01 COUNT

--- SQL_beginner.Day06-1/ex01/day06_ex01.sql
+++ SQL_beginner.Day06-1/ex01/day06_ex01.sql
@@ -1,11 +1,19 @@
 INSERT INTO person_discounts (id, person_id, pizzeria_id, discount)
-SELECT  ROW_NUMBER() OVER() AS id,
-        person_id,
+WITH count_of_orders AS (
+    SELECT  person_id, 
+            pizzeria_id, 
+            COUNT(*) AS count
+    FROM person_order po
+    JOIN menu ON po.menu_id = menu.id
+    GROUP BY po.person_id, menu.pizzeria_id)
+SELECT  ROW_NUMBER() OVER (), 
+        person_id, 
         pizzeria_id,
-        CASE
-            WHEN (SELECT COUNT(*) FROM person_order WHERE person_id = po.person_id) = 1 THEN 10.5
-            WHEN (SELECT COUNT(*) FROM person_order WHERE person_id = po.person_id) = 2 THEN 22
-            ELSE 30
+        CASE 
+            WHEN count = 1 THEN 10.5 
+            WHEN count = 2 THEN 22 
+            ELSE 30 
         END
-FROM person_order po
-JOIN menu ON po.menu_id = menu.id;
+FROM count_of_orders;
+
+--DELETE FROM person_discounts;
