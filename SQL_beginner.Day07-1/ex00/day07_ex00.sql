eef8f4a9b0d715f5617446ce980fd01b8dc6bc27
Анастасия <citadeln@mail.ru>
2024-09-16 16:01:43 +0500

    ex00 create_new_database

--- SQL_beginner.Day07-1/ex00/day07_ex00.sql
+++ SQL_beginner.Day07-1/ex00/day07_ex00.sql
@@ -1,10 +1,6 @@
+-- Active: 1718038112143@@127.0.0.1@5432@pool2@public
 SELECT  person_id,
         COUNT(*) AS count_of_visits
 FROM person_visits
 GROUP BY person_id
 ORDER BY 2 DESC, 1;
-
-SELECT pv.person_id, COUNT(*) AS count_of_visits
-  FROM person_visits pv
- GROUP BY pv.person_id
- ORDER BY count_of_visits DESC, pv.person_id ASC;
