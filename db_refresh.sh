a7d596a2d5191e6123a970c55d34ad464bad32dd
Анастасия <citadeln@mail.ru>
2024-09-20 13:00:29 +0500

    db_refresh.sh

---
+++ db_refresh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# sudo -u monroe dropdb pool2
+# sudo -u monroe createdb pool
+# sudo -u monroe psql -U monroe -d pool < backup.sql
+
+psql -U monroe -d pool -h 127.0.0.1 -p 5432
+createdb pool;
+psql pool < backup.sql;
+dropdb pool --force;
