f20c606798111cfe4f3b532a38335355dcf06b1b
Анастасия <citadeln@mail.ru>
2024-10-02 16:32:57 +0500

    ex00 read commited

--- SQL_beginner.Day08-1/ex00/day08_ex00.sql
+++ SQL_beginner.Day08-1/ex00/day08_ex00.sql
@@ -1,6 +1,23 @@
+-- Provide a proof that your parallel session can’t see your changes until you will make a COMMIT
+
+-- Session #1
+-- update of rating for “Pizza Hut” to 5 points in a transaction mode .
+-- check that you can see a changes in session #1
+
+BEGIN;
 SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+UPDATE pizzeria SET rating = 5 WHERE name = 'Pizza Hut';
+SELECT *, xmin, xmax FROM pizzeria WHERE name = 'Pizza Hut';
 
-BEGIN
-UPDATE pizzeria SET rating = 5 WHERE name = 'Pizza Hut'
-
-COMMIT
+-- Session #2
+-- check that you can’t see a changes in session #2
+BEGIN;
+SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+SELECT *, xmin, xmax FROM pizzeria WHERE name = 'Pizza Hut';
+-- Session #1
+-- publish your changes for all parallel sessions.
+COMMIT;
+-- Session #2
+-- check that you can see a changes in session #2
+COMMIT;
+SELECT *, xmin, xmax FROM pizzeria WHERE name = 'Pizza Hut';
