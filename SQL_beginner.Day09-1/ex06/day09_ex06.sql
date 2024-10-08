6248faaa2d4c9490a30d19b9eb026007a635116b
Анастасия <citadeln@mail.ru>
2024-10-08 20:29:10 +0500

    ex06 red select

--- SQL_beginner.Day09-1/ex06/day09_ex06.sql
+++ SQL_beginner.Day09-1/ex06/day09_ex06.sql
@@ -7,27 +7,18 @@ RETURNS TABLE (
 ) AS
 $$
 BEGIN
-    -- RETURN QUERY
-    -- SELECT pz.name AS pizzeria_name
-    -- FROM person_visits pv
-    -- JOIN person_order po ON pv.person_id = po.person_id
-    -- JOIN pizzeria pz ON pv.pizzeria_id = pz.id
-    -- JOIN menu m ON po.menu_id = m.id AND pz.id = m.pizzeria_id
-    -- JOIN person p ON po.person_id = p.id AND pv.person_id = p.id
-    -- WHERE po.order_date = pdate AND p.name = pperson AND price < pprice;
     RETURN QUERY
     SELECT pz.name AS pizzeria_name
     FROM person_visits pv
-    JOIN pizzeria pz ON pv.pizzeria_id = pz.id
-    JOIN menu m ON pz.id = m.pizzeria_id
-    JOIN person p ON pv.person_id = p.id
-    WHERE po.order_date = pdate AND p.name = pperson AND price < pprice;
+    INNER JOIN pizzeria pz ON pv.pizzeria_id = pz.id
+    INNER JOIN menu m ON pz.id = m.pizzeria_id
+    INNER JOIN person p ON pv.person_id = p.id
+    WHERE visit_date = pdate AND p.name = pperson AND price < pprice;
 END;
 $$ LANGUAGE plpgsql;
 
-DROP FUNCTION fnc_person_visits_and_eats_on_date(varchar, numeric, date);
-
-SELECT * FROM information_schema.routines WHERE routine_type = 'FUNCTION' AND specific_name LIKE 'fnc_%';
+-- DROP FUNCTION fnc_person_visits_and_eats_on_date(varchar, numeric, date);
+-- SELECT * FROM information_schema.routines WHERE routine_type = 'FUNCTION' AND specific_name LIKE 'fnc_%';
 
 select *
 from fnc_person_visits_and_eats_on_date(pprice := 800);
