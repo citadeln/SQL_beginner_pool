e1cb2b5c77dba24de23ca03a8fd7ef0605116492
Анастасия <citadeln@mail.ru>
2024-10-08 18:24:43 +0500

    ex04 RETURNS TABLE

--- SQL_beginner.Day09-1/ex04/day09_ex04.sql
+++ SQL_beginner.Day09-1/ex04/day09_ex04.sql
@@ -1,3 +1,34 @@
+CREATE OR REPLACE FUNCTION fnc_persons_female()
+RETURNS TABLE (
+        id bigint,
+        name varchar,
+        age integer,
+        gender varchar,
+        address varchar
+) AS $$
+        (SELECT * FROM person
+         WHERE person.gender = 'female');
+$$ LANGUAGE sql;
+
+CREATE OR REPLACE FUNCTION fnc_persons_male()
+RETURNS TABLE (
+        id bigint,
+        name varchar,
+        age integer,
+        gender varchar,
+        address varchar
+) AS $$
+        (SELECT * FROM person
+         WHERE person.gender = 'male');
+$$ LANGUAGE sql;
+
+SELECT *
+FROM fnc_persons_male();
+
+SELECT *
+FROM fnc_persons_female();
+
+/*
 CREATE OR REPLACE VIEW v_persons_female AS
 SELECT *
 FROM person p
@@ -8,11 +39,9 @@ SELECT *
 FROM person p
 WHERE p.gender = 'male';
 
-/*
 SELECT table_name
 FROM information_schema.tables
 WHERE table_type = 'VIEW' AND table_schema = 'public';
-*/
 
 CREATE OR REPLACE FUNCTION fnc_persons_male() RETURNS SETOF person AS
 $$
@@ -24,11 +53,6 @@ $$
 SELECT * FROM v_persons_female;
 $$ LANGUAGE sql;
 
-SELECT *
-FROM fnc_persons_male();
-
-SELECT *
-FROM fnc_persons_female();
-
 DROP VIEW IF EXISTS v_persons_female;
 DROP VIEW IF EXISTS v_persons_male;
+*/
